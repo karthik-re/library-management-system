b3f47504c4e8445aa65bddbfa0b3510ca56bfd9e
karthik-re <97017235+karthik-re@users.noreply.github.com>
2022-01-19 14:12:03 +0530

    Changes (#1)
    * Update books.spec.js
    
    * Add files via upload
    
    * Update openapi.json

--- spec/api/books.spec.js
+++ spec/api/books.spec.js
@@ -3,19 +3,51 @@ const app = require("../../app");
 const request = supertest(app);
 
 describe("Books tests", () => {
-    it("should validate book title", async () => {
+
+    //Validating Inputs and input Ids-----------------------------------------------------------
+    it("should validate book body", async () => {
         const response = await request.post("/books").send({});
         expect(response.statusCode).toBe(400);
         expect(response.body.message).toBeDefined();
-        /**
-         * status: 400
-         * {
-         *  "message" : "Title is required"
-         * }
-         */
-        expect(response.body.message).toBe("Title is required");
+        expect(response.body.message).toBe("Body is required");
     });
 
+    it("should validate the bookTitle", async ()=>{
+        const response = await request.post("/books").send({
+            bookAuthor:"George R R Martin"
+        });
+        expect(response.statusCode).toBe(400);
+        expect(response.body.message).toBeDefined();
+        expect(response.body.message).toBe("Title is required");
+    })
+
+    it("should validate the bookAuthor", async ()=>{
+        const response = await request.post("/books").send({
+            bookTitle:"A Game of Thrones"
+        });
+        expect(response.statusCode).toBe(400);
+        expect(response.body.message).toBeDefined();
+        expect(response.body.message).toBe("Author is required");
+    })
+
+    it("should validate the Id inputed by the user",async ()=>{
+        const response = await request.post("/books").send({
+            bookTitle: "A song of ice and fire",
+            bookAuthor: "George R R Martin"
+        });
+        expect(response.statusCode).toBe(200);
+        expect(response.body.bookId).toBeDefined();
+
+        const bookId =response.body.bookId;
+
+        const response2 = await request.get(`/books/${bookId+1}`);
+        expect(response2.statusCode).toBe(404);
+        expect(response2.body.message).toBeDefined;
+        expect(response2.body.message).toBe("Book not found. Please enter valid ID");
+
+    })
+    
+    //POST /books--------------------------------------------------------------------------
     it("should create a book", async () => {
         const response = await request.post("/books").send({
             bookTitle: "A song of ice and fire",
@@ -27,6 +59,7 @@ describe("Books tests", () => {
         expect(response.body.bookAuthor).toBe("George R R Martin");
     });
 
+    //GET /books/:id-----------------------------------------------------------------------
     it("Should return the book by id", async () => {
         const response = await request.post("/books").send({
             bookTitle: "A Game of Thrones",
@@ -42,5 +75,81 @@ describe("Books tests", () => {
         expect(response2.body.bookId).toBe(bookId);
         expect(response2.body.bookTitle).toBe("A Game of Thrones");
         expect(response2.body.bookAuthor).toBe("George R R Martin");
+    });
+
+    //POST /books/:id-----------------------------------------------------------------------
+    it("Should update the book by the id", async ()=>{
+        const response = await request.post("/books").send({
+            bookTitle: "A Clash of Kings",
+            bookAuthor: "George R R Martin"
+        });
+        expect(response.statusCode).toBe(200);
+        expect(response.body.bookId).toBeDefined();
+
+        const bookId = response.body.bookId;
+
+        const response2 = await request.post(`/books/${bookId}`).send({
+            bookTitle: "A Game of thrones",
+            bookAuthor: "George R R Martin"
+        });
+        expect(response2.statusCode).toBe(200);
+        
+        const response3 = await request.get(`/books/${bookId}`);
+        expect(response3.statusCode).toBe(200);
+        expect(response3.body.bookId).toBe(bookId);
+        expect(response3.body.bookTitle).toBe("A Game of Thrones");
+        expect(response3.body.bookAuthor).toBe("George R R Martin");
+    });
+
+    //DELETE /books/:id---------------------------------------------------------------------
+    it("Should delete the book by the id", async ()=>{
+        const response = await request.post("/books").send({
+            bookTitle: "A Clash of Kings",
+            bookAuthor: "George R R Martin"
+        });
+        expect(response.statusCode).toBe(200);
+        expect(response.body.bookId).toBeDefined();
+
+        const bookId = response.body.bookId;
+
+        const response2 = await request.delete(`/books/${bookId}`);
+        expect(response2.statusCode).toBe(200);
+        expect(response.body).toBe("Book is deleted successfully");
+
+        const response3 = await request.get(`/books/${bookId}`);
+        expect(response.statusCode).toBe(404);
+    });
+
+    //GET /books----------------------------------------------------------------------------
+    it("Should get all the books in the library", async ()=>{
+        const response = await request.post("/books").send({
+            bookTitle: "A Clash of Kings",
+            bookAuthor: "George R R Martin"
+        });
+        expect(response.statusCode).toBe(200);
+        expect(response.body.bookId).toBeDefined();
+
+        const bookId1 = response.body.bookId;
+
+        
+        const response2 = await request.post("/books").send({
+            bookTitle: "A song of ice and fire",
+            bookAuthor: "George R R Martin"
+        });
+        expect(response2.statusCode).toBe(200);
+        expect(response2.body.bookId).toBeDefined();
+
+        const bookId2 = response2.body.bookId;
+
+
+        const response3 = await request.get("/books");
+        expect(response3.statusCode).toBe(200);
+        expect(response3.body[0].bookTitle).toBe("A Clash of Kings");
+        expect(response3.body[0].bookAuthor).toBe("George R R Martin");
+        expect(response3.body[0].bookId).toBe(bookId1);
+        expect(response3.body[1].bookTitle).toBe("A song of ice and fire");
+        expect(response3.body[1].bookAuthor).toBe("George R R Martin");
+        expect(response3.body[1].bookId).toBe(bookId2);
     })
+
 })
---
+++ spec/api/history.spec.js
@@ -0,0 +1,139 @@
+const res = require("express/lib/response");
+const supertest = require("supertest");
+const app = require("../../app");
+const request = supertest(app);
+
+describe("History tests",()=>{
+    //GET /books/:id/history------------------------------------------------------------------------------------------------
+    it("Should get the loan history of the book by its ID",async ()=>{
+        const response0 = await request.post("/books").send({
+            bookTitle: "A song of ice and fire",
+            bookAuthor: "George R R Martin"
+        });
+        expect(response0.statusCode).toBe(200);
+        expect(response0.body.bookId).toBeDefined();
+
+        const bookId0 = response0.body.bookId;
+
+        const response = await request.post("/student").send({
+            studentName:"Jonny"
+        });
+        expect(response.statusCode).toBe(200);
+        expect(response.body.studentId).toBeDefined();
+
+        const studentId1 = response.body.studentId;
+
+        const response2 = await request.post("/student").send({
+            studentName:"John"
+        });
+        expect(response2.statusCode).toBe(200);
+        expect(response2.body.studentId).toBeDefined();
+
+        const studentId2 = response2.body.studentId;
+
+        const response3 = await request.post("/loanHistory").send({
+            bookId: bookId0,
+            studentId: studentId1,
+            outDate: "2020-03-22T00:00:00.000Z",
+            returnDate: "2020-03-22T00:00:00.000Z"
+        });
+        expect(response3.statusCode).toBe(200);
+        expect(response3.body.loanId).toBeDefined();
+
+        const loanId1 = response3.body.loanId;
+
+        const response4 = await request.post("/loanHistory").send({
+            bookId: bookId0,
+            studentId: studentId2,
+            outDate: "2020-03-22T00:00:00.000Z",
+            returnDate: "2020-03-22T00:00:00.000Z"
+        });
+        expect(response4.statusCode).toBe(200);
+        expect(response4.body.loanId).toBeDefined();
+
+        const loanId2 = response4.body.loanId;
+
+
+        const res = await request.get(`/books/${bookId0}/history`);
+        expect(res.statusCode).toBe(200);
+        expect(res.body[0].loanId).toBe(loanId1);
+        expect(res.body[1].loanId).toBe(loanId2);
+        expect(res.body[0].student.studentId).toBe(studentId1);
+        expect(res.body[1].student.studentId).toBe(studentId2);
+        expect(res.body[0].student.studentName).toBe("Jonny");
+        expect(res.body[1].student.studentName).toBe("John");
+        expect(res.body[0].outDate).toBe("2020-03-22T00:00:00.000Z");
+        expect(res.body[1].outDate).toBe("2020-03-22T00:00:00.000Z");
+        expect(res.body[0].returnDate).toBe("2020-03-22T00:00:00.000Z");
+        expect(res.body[1].returnDate).toBe("2020-03-22T00:00:00.000Z");
+
+    });
+
+    //GET /student/:id/history--------------------------------------------------------------------------------------------------
+    it("Should get the loan history of the student by their ID",async ()=>{
+        const response0 = await request.post("/books").send({
+            bookTitle: "A song of ice and fire",
+            bookAuthor: "George R R Martin"
+        });
+        expect(response0.statusCode).toBe(200);
+        expect(response0.body.bookId).toBeDefined();
+
+        const bookId1 = response0.body.bookId;
+
+        const response = await request.post("/books").send({
+            bookTitle: "A Game of Thrones",
+            bookAuthor: "George R R Martin"
+        });
+        expect(response.statusCode).toBe(200);
+        expect(response.body.bookId).toBeDefined();
+
+        const bookId2 = response.body.bookId;
+
+        const response2 = await request.post("/student").send({
+            studentName:"John"
+        });
+        expect(response2.statusCode).toBe(200);
+        expect(response2.body.studentId).toBeDefined();
+
+        const studentId = response2.body.studentId;
+
+        const response3 = await request.post("/loanHistory").send({
+            bookId: bookId1,
+            studentId: studentId,
+            outDate: "2020-03-22T00:00:00.000Z",
+            returnDate: "2020-03-22T00:00:00.000Z"
+        });
+        expect(response3.statusCode).toBe(200);
+        expect(response3.body.loanId).toBeDefined();
+
+        const loanId1 = response3.body.loanId;
+
+        const response4 = await request.post("/loanHistory").send({
+            bookId: bookId2,
+            studentId: studentId,
+            outDate: "2020-03-22T00:00:00.000Z",
+            returnDate: "2020-03-22T00:00:00.000Z"
+        });
+        expect(response4.statusCode).toBe(200);
+        expect(response4.body.loanId).toBeDefined();
+
+        const loanId2 = response4.body.loanId;
+
+
+        const res = await request.get(`/student/${studentId}/history`);
+        expect(res.statusCode).toBe(200);
+        expect(res.body[0].loanId).toBe(loanId1);
+        expect(res.body[1].loanId).toBe(loanId2);
+        expect(res.body[0].book.bookId).toBe(bookId1);
+        expect(res.body[1].book.bookId).toBe(bookId2);
+        expect(res.body[0].book.bookTitle).toBe("A song of ice and fire");
+        expect(res.body[1].book.bookTitle).toBe("A Game of Thrones");
+        expect(res.body[0].book.bookAuthor).toBe("George R R Martin");
+        expect(res.body[1].book.bookAuthor).toBe("George R R Martin");
+        expect(res.body[0].outDate).toBe("2020-03-22T00:00:00.000Z");
+        expect(res.body[1].outDate).toBe("2020-03-22T00:00:00.000Z");
+        expect(res.body[0].returnDate).toBe("2020-03-22T00:00:00.000Z");
+        expect(res.body[1].returnDate).toBe("2020-03-22T00:00:00.000Z");
+    });
+
+});
