e448247d3a7b424905322ee31d7fdc638f5c5539
karthik-re <97017235+karthik-re@users.noreply.github.com>
2022-01-19 19:34:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openApiDocumentation));
 
+
+app.use('/books',require('./books/router'));
+
 app.get('/',(req,res)=>{
   res.status(200).send(`Welcome!. Please got to <a href="/api-docs">docs</a> to see swagger UI`)
 })
